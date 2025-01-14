09f2e4f710db39acda7576013937757f16c7ddfd
anonymous6299 <ethicalanonymousgreyhat@gmail.com>
2025-01-15 00:04:10 +0530

    Day3done

--- Day3/script.js
+++ Day3/script.js
@@ -1,48 +1,69 @@
 const id = 237;
-const margins=["23rem","-67rem","-157rem","-247rem","-337rem"]
 const images = document.getElementById("slider");
+var MaxMargin=-200;
+var MarCh=50;
+
+
+
 const imgArr = Array.from({length:5},(_,index)=>{
     return `https://picsum.photos/id/${id+index}/800/400`
 })
-
 images.innerHTML=`${
     imgArr.map((item)=>{
-        return `<img src=${item} alt="img" style="margin-right: 40rem;">`
+        return `<img src=${item} alt="img" style="margin-left:0rem">`
     })
 }`.split(",").join(" ")
 
+
+
+const ImageToBeMoved = images.firstElementChild;
+ImageToBeMoved.style.transition="margin-left 0.4s";
+window.addEventListener(('resize'),()=>{
+    ImageToBeMoved.style.marginLeft="0rem"
+    if (window.innerWidth<=880) {
+        MarCh=37.5;
+        MaxMargin=-150
+    }
+    if (window.innerWidth<=680) {
+        MarCh=25;
+        MaxMargin=-100;
+    }
+    if (window.innerWidth<=420){
+        MarCh=12.5;
+        MaxMargin=-50;
+    }
+
+})
+
 const moveToNext = () => {
-    const left = images.style.marginLeft;
-    const pos = margins.findIndex((item,index)=>{
-    return item===left})
-    if (pos+1>4) {
-        images.style.marginLeft=margins[0];
+    const left = parseFloat(ImageToBeMoved.style.marginLeft);
+    if (left===MaxMargin) {
+        ImageToBeMoved.style.marginLeft="0rem";
     }
     else{
-        images.style.marginLeft=margins[pos+1];
+        ImageToBeMoved.style.marginLeft=`${left-MarCh}rem`;
     }
 
 }
 const moveToPrev = () => {
-    const left = images.style.marginLeft;
-    const pos = margins.findIndex((item,index)=>{
-    return item===left})
-    if (pos-1<0) {
-        images.style.marginLeft=margins[4];
+    const left = parseFloat(ImageToBeMoved.style.marginLeft);
+    if (left===0) {
+        ImageToBeMoved.style.marginLeft=`${MaxMargin}rem`;
     }
     else{
-        images.style.marginLeft=margins[pos-1];
+        ImageToBeMoved.style.marginLeft=`${left+MarCh}rem`;
     }
+
 }
-const getJokes = async() => {
-    const resp = await (await fetch("https://api.api-ninjas.com/v1/jokes",{
-        method:'GET',
-        headers:{
-            'Content-Type':'application/json',
-            'X-Api-Key':"lvPZGD/xEpENnSl9ySPYAw==mhNpUWfac1poH14a"
-        }
-    })).json();
-    const [response] = resp
-    document.getElementById("joke").innerText=response.joke
-}
-getJokes();
+// const getJokes = async() => {
+//     const resp = await (await fetch("https://api.api-ninjas.com/v1/jokes",{
+//         method:'GET',
+//         headers:{
+//             'Content-Type':'application/json',
+//             'X-Api-Key':"lvPZGD/xEpENnSl9ySPYAw==mhNpUWfac1poH14a"
+//         }
+//     })).json();
+//     const [response] = resp
+//     document.getElementById("joke").innerText=response.joke
+// }
+// getJokes();
