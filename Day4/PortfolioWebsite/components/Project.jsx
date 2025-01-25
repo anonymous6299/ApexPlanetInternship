fc10c2ecbfa4e12993505e8733ac948ff81c9bef
anonymous6299 <ethicalanonymousgreyhat@gmail.com>
2025-01-25 15:01:56 +0530

    RespForPrtflWeb

--- Day4/PortfolioWebsite/components/Project.jsx
+++ Day4/PortfolioWebsite/components/Project.jsx
@@ -7,7 +7,7 @@ const Project = (props) => {
         <div className="projectCard rounded-lg w-[22rem] py-3 border border-black hover:shadow-lg cursor-pointer">
             <img src={project.imageURL} alt="logo" className='h-56 w-full px-10' />
             <hr className='my-4 border-gray-300' />
-            <div className="desc px-4 grid grid-rows-5">
+            <div className="desc px-4 grid" style={{gridTemplateRows:"2rem 3.5rem 3.5rem 2rem 1.5rem"}}>
                 {
                     fields.map((item,index)=>{
                         return <div className="grid grid-cols-2">
@@ -18,10 +18,10 @@ const Project = (props) => {
                 }
                 <div className="grid grid-cols-2">
                     <p className="font-medium">Links:</p>
-                    <div className="flex items-center space-x-3 underline text-blue-800">
-                        <a href="#">Live</a>
+                    <div className="flex items-center space-x-3">
+                        <a href="#" className="text-blue-800 hover:underline">Live</a>
                         <p>|</p>
-                        <a href="#">GitHub</a>
+                        <a href="#" className="text-blue-800 hover:underline">GitHub</a>
                     </div>
                 </div>
             </div>
--- Day4/PortfolioWebsite/components/Projects.jsx
+++ Day4/PortfolioWebsite/components/Projects.jsx
@@ -7,6 +7,7 @@ const Projects = () => {
   return (
     <>
       <NavBar />
+      <h1 className='font-bold text-4xl text-center mt-10'>Projects</h1>
       <div className="grid grid-cols-3 grid-rows-2 pl-44 pr-40 py-20" style={{rowGap:'4rem'}}>
         {ProjectsData.map((project, index) => (
           <Project key={index} project={project} />))}
